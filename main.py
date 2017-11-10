14c743c86df69894771c54267cf20e9f97b60ebd
FracturedRetina <eshimoniak@gmail.com>
2017-11-09 20:06:15 -0800

    I'm about to change some stuff so I guess I should commit

--- main.py
+++ main.py
@@ -1,23 +1,66 @@
 from selenium import webdriver
 from movie import *
+from random import randint
+import os.path
 
+loadtype = 'r'
 browser = webdriver.PhantomJS()
 
 movies = []
 
-f = open("movies.txt", "r")
 
-print("Loading movies...")
-for line in f:
-	line = line[:-1]
-	print("\tLoading \"{0}\"...".format(line), end="")
-	movies.append(movie_by_name(browser, line))
-	print("Done!")
-print("Done!")
+if loadtype == 'f':
+	for i in range(2017, 2007, -1):
+		f = open("res/{0}.txt".format(i), "r")
 
-print("IMDB id\trating\t10*\t1*")
-print("-------------------------------")
-for movie in movies:
-	print("{0}\t{1}\t{2}\t{3}".format(movie.imdb_id, movie.get_rating(browser), movie.get_n_star(browser, 10), movie.get_n_star(browser, 1)))
-
+		print("Loading movies from {0}...".format(i))
+		for line in f:
+			line = line[:-1]
+			print("\tLoading \"{0}\"...".format(line), end="")
+			movie = movie_by_name(browser, line)
+			if movie != None:
+				movies.append(movie)
+				print("Done!")
+			else:
+				print("Error :(")
+		print("Done!")
+		
+		print("IMDB id\trating\t10*\t1*")
+		print("-------------------------------")
+		for movie in movies:
+			print("{0}\t{1}\t{2}\t{3}".format(movie.imdb_id, movie.get_rating(browser), movie.get_n_star(browser, 10), movie.get_n_star(browser, 1)))
+elif loadtype == 'r':
+	nummovies = 5
+	for i in range(2017, 2007, -1):
+		print("Selecting {0} random movies from {1}...".format(nummovies, i))
+		f = open("res/{0}.txt".format(i))
+		lines = f.readlines()
+		f.close()
+		selected = [None]
+		while len(movies) - (2017 - i) * nummovies < nummovies:
+			title = None
+			while title in selected:
+				title = lines[randint(0, len(lines) - 1)][:-1]
+			m = movie_by_name(browser, title)
+			if m.get_num_ratings(browser) >= 5000:
+				movies.append(m)
+				print("\tSelected \"{0}\"".format(title))
+		print("Done!")
+	
+	fnum = 0
+	while os.path.exists("log{:03}.csv".format(fnum)):
+		fnum += 1
+	
+	f = open("log{:03}.csv".format(fnum), "w")
+	
+	print("Title,IMDb ID,Rating,10 Star,1 Star", file=f)
+	for m in movies:
+		title = m.get_title(browser)
+		imdb_id = m.imdb_id
+		rating = m.get_rating(browser)
+		ten_star = m.get_n_star(browser, 10)
+		one_star = m.get_n_star(browser, 1)
+		print("{0},{1},{2},{3},{4}".format(title, imdb_id, rating, ten_star, one_star), file=f)
+		print("{0}\t{1}\t{2}\t{3}\t{4}".format(title, imdb_id, rating, ten_star, one_star))
+	
 browser.quit()
--- movie.py
+++ movie.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 class Movie:
-	def __init__(self, imdb_id):
-		if imdb_id[:2] == "tt":
-			imdb_id = imdb_id[2:]
+	def __init__(self, imdb_id, title=None):
+		if str(imdb_id)[:2] == "tt":
+			imdb_id = str(imdb_id[2:])
 		
 		self.imdb_id = imdb_id
 	
@@ -18,43 +19,81 @@ class Movie:
 	def load_page(self, driver, page, verbose=False):
 		url = None
 		if page == "ratings" and not self.get_page(driver) == "ratings":
-			url = "http://www.imdb.com/title/tt%s/ratings" % self.imdb_id
+			url = "http://www.imdb.com/title/tt{0}/ratings".format(self.imdb_id)
 		elif page == "title" and not self.get_page(driver) == "title":
-			url = "http://www.imdb.com/title/tt%s" % self.imdub_id
-		
+			url = "http://www.imdb.com/title/tt{0}".format(self.imdb_id)
 		if url != None:
 			if verbose:
 				print("Accessing \"{0}\"...".format(url), end="")
 			driver.get(url)
+			
+			try:
+				WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "navbar-query")))
+			except TimeoutException:
+				pass
+			
 			if verbose:
 				print("Done!")
+	
+	def get_title(self, driver, verbose=False):
+		if self.title == None:
+			if self.get_page(driver, verbose) == "title":
+				self.title = driver.find_elements_by_path("//h1[0]").get_attribute("innerText")
+			else:
+				self.load_page(driver, "title", verbose)
+				self.title = self.get_title(driver, verbose)
+		
+		return self.title
 	
 	def has_ratings(self, driver, verbose=False):
 		if self.get_page(driver) == "ratings":
 			return len(driver.find_elements_by_xpath("//tbody[1]/tr")) >= 11
 		else:
 			self.load_page(driver, "ratings", verbose)
-			return self.has_ratings(driver)
+			return self.has_ratings(driver, verbose)
 	
 	def get_n_star(self, driver, stars, verbose=False):
 		if self.has_ratings(driver):
 			self.load_page(driver, "ratings", verbose)
 		
-			return driver.find_element_by_xpath("//tbody[1]/tr[{0}]/td".format(12 - stars)).get_attribute("innerText")
+			return int(driver.find_element_by_xpath("//tbody[1]/tr[{0}]/td".format(12 - stars)).get_attribute("innerText"))
 	
-	def get_rating(self, driver, verbose=False):
+	def get_num_ratings(self, driver, verbose=False):
 		if self.get_page(driver) == "ratings":
-			return driver.find_element_by_xpath("//div[@id=\"tn15content\"]/p/a[2]").get_attribute("innerText")
+			if self.has_ratings(driver, verbose):
+				num_ratings = 0
+				for i in range (1, 11):
+					num_ratings += self.get_n_star(driver, i, verbose)
+				return num_ratings
+			else:
+				return 0
+		elif self.get_page(driver) == "title":
+			return int(driver.find_element_by_xpath("//*[@id=\"title-overview-widget\"]/div[2]/div[2]/div/div[1]/div[1]/a/ratings").get_attribute("innerText"))
 		else:
 			self.load_page(driver, "ratings", verbose)
-			return self.get_rating(driver, verbose)
+			return self.get_num_ratings(driver, verbose)
+	
+	def get_rating(self, driver, verbose=False):
+		if self.has_ratings(driver):
+			if self.get_page(driver) == "ratings":
+				return driver.find_element_by_xpath("//div[@id=\"tn15content\"]/p/a[2]").get_attribute("innerText")
+			else:
+				self.load_page(driver, "ratings", verbose)
+				return self.get_rating(driver, verbose)
 
 def movie_by_name(driver, name):
 	if not "imdb.com" in driver.current_url:
 		driver.get("http://www.imdb.com")
+		try:
+			WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "navbar-query")))
+		except TimeoutException:
+			return None
 	searchbar = driver.find_element_by_id("navbar-query")
 	searchbar.clear()
 	searchbar.send_keys(name)
 	searchbar.submit()
 	
-	return Movie(WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "td.result_text > a"))).get_attribute("href")[26:35])
+	try:
+		return Movie(WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "td.result_text > a"))).get_attribute("href")[26:35], title=name)
+	except TimeoutException:
+		return None
